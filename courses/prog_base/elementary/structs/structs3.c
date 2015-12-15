e0e14e08207b716b590f679d479d8370c605b959
KarinaChumak <karinac3011@gmail.com>
2015-12-15 17:05:23 +0200

    elementary structs redone

--- courses/prog_base/elementary/structs/structs3.c
+++ courses/prog_base/elementary/structs/structs3.c
@@ -1,8 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
-//як викликати функц≥ю в main?? якщо пишу так,€к закоментовано, видаЇ помилку "conflicting types"
-//double area (struct rectangular *rect);
 
 struct koord {
     int x;
@@ -15,39 +13,28 @@ struct rectangular {
     int height;
 };
 
-
+double area (struct rectangular *rect);
 int main (){
     struct rectangular rect[3]={5,3,28,10,    8,6,21,9,   7,12,10,2};
     for(int i=0;i<3;i++){
     printf(" Coordinates of first point :(%i,%i), length:%i, height:%i\n" , rect[i].p1.x, rect[i].p1.y, rect[i].length, rect[i].height );
     }
-/*int i;
-area(&rect[i]);*/
 
-double res;
+area(rect);
+
+  return 0;
+}
+
+double area (struct rectangular *rect){
+ double res;
  for(int i=0;i<3;i++){
  res=rect[i].length*rect[i].height;
- printf("%darea = %.0f; ", i+1, res );
- }
+ printf("%darea = %.0f; ", i+1, res );}
+
  double minArea=0;
  for(int i=1;i<3;i++){
      minArea=rect[0].length*rect[0].height;
     if (rect[i].length*rect[i].height<rect[0].length*rect[0].height)
         minArea=rect[i].length*rect[i].height;
  }
-  printf("\nMinimal area = %.0f; ",  minArea );
-
-
-  return 0;
-}
-
-
-/*double area (struct rectangular *rect){
- double res;
- for(int i=0;i<3;i++){
- res=rect[i].length*rect[i].height;
- printf("%darea = %.0f; ", i+1, res );
-
-
-}*/
-
+  printf("\nMinimal area = %.0f; ",  minArea );}
